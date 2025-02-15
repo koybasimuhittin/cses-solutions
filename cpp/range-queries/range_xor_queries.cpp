06335793137fd7d2870238d40e4d4fd9ed080d87
koybasimuhittin <koybasimuhittin@gmail.com>
2025-02-15 14:44:03 +0300

    [cpp] range xor queries

---
+++ cpp/range-queries/range_xor_queries.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define f1 first
+#define s2 second
+#define pb push_back
+#define mp make_pair
+#define int long long
+#define fri(a) freopen(a, "r", stdin);
+#define fro(a) freopen(a, "w", stdout);
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 5;
+
+int n, m, t, arr[N];
+
+int32_t main() {
+    // fri("in.txt");
+    // fro("out.txt");
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        arr[i] ^= arr[i - 1];
+    }
+
+    int l, r;
+    while (m--) {
+        cin >> l >> r;
+        cout << (arr[r] ^ arr[l - 1]) << endl;
+    }
+
+    return 0;
+}
