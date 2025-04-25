9cb2e8880d170f68f07562412f9265ca31e3acaa
koybasimuhittin <koybasimuhittin@gmail.com>
2025-04-25 14:15:26 +0300

    [cpp] nim game ii

---
+++ cpp/mathematics/nim_game_ii.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+#define f1 first
+#define s2 second
+#define pb push_back
+#define mp make_pair
+#define int long long
+#define fri(a) freopen(a, "r", stdin);
+#define fro(a) freopen(a, "w", stdout);
+const int MOD = 1e9 + 7;
+const int N = 2e5 + 5;
+
+int n, m, t, arr[N];
+vector<int> v;
+
+int32_t main() {
+    // fri("in.txt");
+    // fro("out.txt");
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> t;
+
+    while (t--) {
+        cin >> n;
+        int ans = 0, a;
+        for (int i = 0; i < n; i++) {
+            cin >> a;
+            ans ^= (a % 4);
+        }
+
+        cout << (ans == 0 ? "second" : "first") << endl;
+    }
+
+    return 0;
+}
