c370675031c73f2f2eee5a1df081f6ca6c0e69b6
koybasimuhittin <koybasimuhittin@gmail.com>
2025-04-26 21:01:04 +0300

    [cpp-fix] meet in the middle

--- cpp/advanced-techniques/meet_in_the_middle.cpp
+++ cpp/advanced-techniques/meet_in_the_middle.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 #define endl '\n'
@@ -14,9 +13,9 @@ const int MOD = 1e9 + 7;
 const int N = 41;
 
 int n, m, arr[N];
-unordered_map<int, int> cnt;
 
 int32_t main() {
+
     // fri("in.txt");
     // fro("out.txt");
 
@@ -24,36 +23,59 @@ int32_t main() {
     cin.tie(NULL);
 
     cin >> n >> m;
+    for (int i = 0; i < n; i++) cin >> arr[i];
 
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    int n1 = n / 2, n2 = n - n1;
+    vector<int> top_sums;
+    top_sums.reserve(1 << n1);
+    for (int mask = 0; mask < (1 << n1); mask++) {
+        int sum = 0, x = mask;
+        while (x) {
+            int b = __builtin_ctzll(x);
+            sum += arr[b];
+            x &= x - 1;
+        }
+        top_sums.pb(sum);
     }
+
+    vector<int> bottom_sums;
+    bottom_sums.reserve(1 << n2);
+    for (int mask = 0; mask < (1 << n2); mask++) {
+        int sum = 0, x = mask;
+        while (x) {
+            int b = __builtin_ctzll(x);
+            sum += arr[n1 + b];
+            x &= x - 1;
+        }
+        bottom_sums.pb(sum);
+    }
+
+    sort(top_sums.begin(), top_sums.end());
+    sort(bottom_sums.begin(), bottom_sums.end());
 
     int ans = 0;
-
-    for (int mask = 0; mask < (1 << (n / 2)); mask++) {
-        int sum = 0;
-        for (int i = 0; i < n / 2; i++) {
-            if (mask & (1 << i)) {
-                sum += arr[i];
-            }
+    int i = 0, j = (int)bottom_sums.size() - 1;
+    while (i < (int)top_sums.size() && j >= 0) {
+        long long s = (long long)top_sums[i] + bottom_sums[j];
+        if (s < m) {
+            i++;
         }
-        cnt[sum]++;
-    }
-
-    for (int mask = 0; mask < (1 << (n - n / 2)); mask++) {
-        int sum = 0;
-        for (int i = 0; i < n - n / 2; i++) {
-            if (mask & (1 << i)) {
-                sum += arr[i + n / 2];
-            }
+        else if (s > m) {
+            j--;
         }
-        if (m - sum >= 0) {
-            ans += cnt[m - sum];
+        else {
+            long long a = top_sums[i], b = bottom_sums[j];
+            long long c1 = 0, c2 = 0;
+            while (i < (int)top_sums.size() && top_sums[i] == a) {
+                c1++; i++;
+            }
+            while (j >= 0 && bottom_sums[j] == b) {
+                c2++; j--;
+            }
+            ans += c1 * c2;
         }
     }
 
     cout << ans << endl;
-
     return 0;
 }
